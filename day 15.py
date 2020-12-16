15f0106c40c66044da14535745af4536992c5676
Daniel Ready <Dan Ready>
2020-12-16 12:05:59 -0500

    maybe clearer?

--- day 15.py
+++ day 15.py
@@ -1,23 +1,23 @@
-def recite(starting_ages, until_epoch):
-    epoch = len(starting_ages.keys()) + 1
-    turn = 0
+def recite(recited_number_ages, until_epoch):
+    epoch = len(recited_number_ages.keys()) + 1
+    recited_number = 0
     while epoch < until_epoch:
-        if turn in starting_ages.keys():
-            new_turn = epoch - starting_ages[turn]
-            starting_ages[turn] = epoch
-            turn = new_turn
+        if recited_number in recited_number_ages.keys():
+            next_number_to_recite = epoch - recited_number_ages[recited_number]
+            recited_number_ages[recited_number] = epoch
+            recited_number = next_number_to_recite
         else:
-            starting_ages[turn] = epoch
-            turn = 0
+            recited_number_ages[recited_number] = epoch
+            recited_number = 0
         epoch += 1
-    print(str(epoch) + ': ' + str(turn))
+    print(str(epoch) + ': ' + str(recited_number))
 
 # starting state
-ages = {14: 1, 8: 2, 16: 3, 0: 4, 1: 5, 17: 6}
+recited_number_ages = {14: 1, 8: 2, 16: 3, 0: 4, 1: 5, 17: 6}
 
-# part 1 and 2
-recite(ages, 2020)
+# part 1
+recite(recited_number_ages, 2020)
 
-# reset
-ages = {14: 1, 8: 2, 16: 3, 0: 4, 1: 5, 17: 6}
-recite(ages, 30000000)
+# reset and part 2
+recited_number_ages = {14: 1, 8: 2, 16: 3, 0: 4, 1: 5, 17: 6}
+recite(recited_number_ages, 30000000)

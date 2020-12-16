62a0b5a35ad624f6edb1bba2d137fd379533d1d7
Daniel Ready <Dan Ready>
2020-12-16 17:28:05 -0500

    maybe epoch is not the clearest variable name.

--- day 15.py
+++ day 15.py
@@ -1,16 +1,16 @@
-def recite(recited_number_ages, until_epoch):
-    epoch = len(recited_number_ages.keys()) + 1
+def recite(recited_number_ages, final_turn):
+    turn = len(recited_number_ages.keys()) + 1
     recited_number = 0
-    while epoch < until_epoch:
+    while turn < final_turn:
         if recited_number in recited_number_ages.keys():
-            next_number_to_recite = epoch - recited_number_ages[recited_number]
-            recited_number_ages[recited_number] = epoch
+            next_number_to_recite = turn - recited_number_ages[recited_number]
+            recited_number_ages[recited_number] = turn
             recited_number = next_number_to_recite
         else:
-            recited_number_ages[recited_number] = epoch
+            recited_number_ages[recited_number] = turn
             recited_number = 0
-        epoch += 1
-    print(str(epoch) + ': ' + str(recited_number))
+        turn += 1
+    print(str(turn) + ': ' + str(recited_number))
 
 # starting state
 recited_number_ages = {14: 1, 8: 2, 16: 3, 0: 4, 1: 5, 17: 6}
